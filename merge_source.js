97289a043b7f75db183e3fc9b194476c0c00c582
Brian O'Donovan <bodonovan@gmail.com>
2024-05-17 10:48:24 +0100

    Fixed problem with mering files

--- merge_source.js
+++ merge_source.js
@@ -12,37 +12,39 @@ fs.readFile(inputFile, 'utf8', (err, data) => {
     console.error('Error reading input file:', err);
     return;
   }
+  fs.readFile(inputFile2, 'utf8', (err2, data2) => {
+    if (err2) {
+      console.error('Error reading input file:2', err2);
+      return;
+    }
 
   // Split data into lines
   const lines = data.trim().split('\n');
 
-  // Write lines to output file
+  // Split data2 into lines
+    console.warn("Split data2 into lines");
+    const lines2 = data2.trim().split('\n');
+
+  // Open  output file
   const outputStream = fs.createWriteStream(outputFile);
+  // outputStream.flush();
   lines.forEach(line => {
     if (line == 'DICT_GOES_HERE') {
-      // outputStream.write(`here goes something\n`);
-      fs.readFile(inputFile2, 'utf8', (err, data2) => {
-        if (err) {
-          console.error('Error reading input file:2', err);
-          return;
-        }
-      
-        // Split data into lines
-        const lines2 = data2.trim().split('\n');
-
-        // whrite to output file
-        for (const line2 in lines2) {
-          outputStream.write(line2+`\n`);    
-        }
-      });
-      
+        // write dict.js to output file
+        // for (const line2 in lines2) {
+        lines2.forEach(line2 => {
+          outputStream.write(line2+`\n`);  
+          console.log('Writing data line', line2);  
+        });
     } else {
+      console.log('Writing HTML line', line);  
       outputStream.write(line+`\n`);
+      // outputStream.flush();
     }
   });
 
   // Close output stream
   outputStream.end();
-
   console.log('Lines written to output file successfully.');
+});
 });
